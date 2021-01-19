72d89883c2b3441726a16854f22047a2f6658ee6
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-01-19 11:34:24 +0100

    Detail the main

--- actix-web/src/main.rs
+++ actix-web/src/main.rs
@@ -1,12 +1,14 @@
 // On separate lines on purpose so I can add comments later or sooner
 use actix_web::get;
 use actix_web::post;
+use actix_web::App;
 use actix_web::HttpResponse;
+use actix_web::HttpServer;
 use actix_web::Responder;
 
 // Request handlers are all async functions.
 //
-// "get" here is an attribute-like macro:
+// "get" here is an attribute-like macro (`proc_macro_attribute`):
 // https://doc.rust-lang.org/book/ch19-06-macros.html?highlight=attribute#attribute-like-macros
 // It is a way for Actix to reduce boilerplate code in Actix Web applications.
 // The code is here: https://github.com/actix/actix-web/blob/master/actix-web-codegen/src/lib.rs
@@ -41,6 +43,23 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-fn main() {
-    println!("Starting...");
+// Here we see another use of `proc_macro_attribute`, implemented here:
+// https://github.com/actix/actix-web/blob/0c8196f8b07022e56fb4f967c26426cd3076ffdf/actix-web-codegen/src/lib.rs#L169
+//
+// It is syntactic sugar on top of `actix_web::rt::System::new`, which is an alias to:
+// https://docs.rs/actix-rt/1.1.1/actix_rt/ System struct.
+//
+// actix-rt (https://crates.io/crates/actix-rt) is a:
+// "Tokio-based single-thread async runtime for the Actix ecosystem"
+//
+// Tokio is an [asynchronous runtime for Rust](https://tokio.rs).
+#[actix_web::main]
+// TODO: detail the full chain (bind/run/await) and most importantly why we need this return type
+async fn main() -> std::io::Result<()> {
+    // The || { } construct is just a zero-parameter closure. It allows to
+    // defer the evaluation to later (at the appropriate time).
+    HttpServer::new(|| App::new())
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
